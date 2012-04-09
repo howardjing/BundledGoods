d3f3d76b16d5e9cf18c598f432a46656a30fd2d7
Howard Jing <howard.jing@gmail.com>
2012-04-09 01:19:13 -0400

    question navigation

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,4 +4,37 @@ class Question < ActiveRecord::Base
   belongs_to :survey
   
   validates_presence_of :content
+  
+  # ==== Methods ====
+  delegate :url_helpers, to: 'Rails.application.routes' 
+  def position
+    self.survey.questions.index(self)
+  end
+  
+  def is_first?
+    self.position == 0
+  end
+  
+  def is_last?
+    self.position == self.survey.questions.count - 1
+  end
+  
+  def next
+    self.survey.questions[self.position+1]
+  end
+  
+  def previous
+    self.survey.questions[self.position-1]
+  end
+  
+  def next_path
+    self.is_last? ? url_helpers.surveys_path : url_helpers.survey_question_path(self.survey, self.next)
+  end
+  
+  def previous_path
+    self.is_first? ? url_helpers.survey_path(self.survey) : url_helpers.survey_question_path(self.survey, self.previous)
+  end
+    
+    
+  
 end
--- app/models/survey.rb
+++ app/models/survey.rb
@@ -2,4 +2,8 @@ class Survey < ActiveRecord::Base
   
   has_many :questions, :dependent => :destroy
   validates_presence_of :name
+  
+  def first_question
+    self.questions.first
+  end
 end
