abd687d095d14f143663ace1abb613076dade54d
Howard Jing <howard.jing@gmail.com>
2012-10-29 14:52:24 -0400

    Not entirely sure I follow with the value / utility changes

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,7 +1,7 @@
 module QuestionsHelper
   
   def menu_name(obj)
-    "#{obj.menu_name}: #{number_to_currency obj.price}"
+    "#{obj.menu_name}: #{number_to_currency obj.price} for a total value of #{obj.value} utility - #{number_to_currency obj.price} = #{obj.value - obj.price}"
   end
 
   def next_path(question)
