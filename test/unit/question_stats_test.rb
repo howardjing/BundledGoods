c2f630c6b1e658acb51464f2b9e7a0905c3b273d
Howard Jing <howard.jing@gmail.com>
2013-03-18 22:09:37 -0400

    adding test to make sure that the chosen question is not the demo question

--- test/unit/question_stats_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class QuestionStatsTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  fixtures :instructions
   
   # must assign user, duration, and number of goods
   test "#build_random_question assigns the parameters correctly" do
@@ -9,5 +10,15 @@ class UserTest < ActiveSupport::TestCase
     assert_equal question.user, user
     assert_equal question.values.length, 3
     assert_equal question.duration, 100
+  end
+  
+  # make sure that the chosen_question was not the demo question
+  test "#generate_questions" do
+    100.times do |i|
+      user = User.new
+      user.lab_number = i
+      user.save
+      assert_equal false, user.chosen_question.demo? 
+    end
   end
 end
