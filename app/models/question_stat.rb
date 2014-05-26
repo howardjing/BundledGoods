b0fbcb079d5b844892a24809571601b2e664a3ae
Howard Jing <howard.jing@gmail.com>
2014-05-26 01:26:48 -0400

    adding QuestionStat#partial_value and QuestionStat#statement_value

--- app/models/question_stat.rb
+++ app/models/question_stat.rb
@@ -41,6 +41,18 @@ class QuestionStat < ActiveRecord::Base
     stat_type == 'Good'
   end
 
+  def menu_item?
+    stat_type == 'Menu'
+  end
+
+  def statement_shown?
+    menu_item? && content.starts_with?(STATEMENT_SHOWN_PREFIX)
+  end
+
+  def statement_goods
+    content.delete(STATEMENT_SHOWN_PREFIX).split(",").map(&:to_i)
+  end
+
   # if the content ends in true, then it was selected
   def selected?
     content.rpartition(" ").last == 'true'
@@ -52,4 +64,107 @@ class QuestionStat < ActiveRecord::Base
     end
   end
 
+  def statement_value
+    value_analyzer.statement_value
+  end
+
+  def partial_value
+    value_analyzer.partial_value
+  end
+
+  private
+
+  def value_analyzer
+    if combo?
+      @value_analyzer ||= ComboAnswer.new(self)
+    elsif good?
+      @value_analyzer ||= GoodAnswer.new(self)
+    elsif menu_item?
+      @value_analyzer ||= MenuItem.new(self)
+    else
+      raise StandardError.new("PROGRAMMING ERROR!!!! INVALID QUESITON STAT")
+    end
+  end
+
+  class GoodAnswer
+    attr_reader :stat
+    def initialize(stat)
+      @stat = stat
+    end
+
+    # answers don't have a statement value
+    def statement_value
+      nil
+    end
+
+    # compute selected values up to now
+    def partial_value
+      @partial_value ||= compute_partial_value
+    end
+
+    private
+
+    def compute_partial_value
+      active_goods = Set.new
+      preceeding_selections.each do |selection|
+        if selection.combo?
+          active_goods = Set.new # reset active goods
+        elsif selection.good?
+          if selection.selected?
+            active_goods.add(selection.good_number)
+          else
+            active_goods.delete(selection.good_number)
+          end
+        else
+          raise StandardError.new("PROGRAMMING ERROR: examine QuestionStat #{stat.id}")
+        end
+      end
+      stat.question.bundle(active_goods.to_a.sort)
+    end
+
+    # preceeding selections includes current selection
+    def preceeding_selections
+      @preceeding_selections ||= stat.question.question_stats.selections
+                                     .where('created_at <= ?', stat.created_at)
+    end
+  end
+
+  class ComboAnswer
+    attr_reader :stat
+    def initialize(stat)
+      @stat = stat
+    end
+
+    def statement_value
+      nil
+    end
+
+    def partial_value
+      @partial_value ||= if stat.selected?
+        stat.question.combo_witheffect
+      else
+        0
+      end
+    end
+  end
+
+  class MenuItem
+    attr_reader :stat
+    def initialize(stat)
+      @stat = stat
+    end
+
+    def statement_value
+      @statement_value ||= if stat.statement_shown?
+        stat.question.bundle(stat.statement_goods)
+      else
+        0
+      end
+    end
+
+    # menu interactions don't have a partial value
+    def partial_value
+      nil
+    end
+  end
 end
